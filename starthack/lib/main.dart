d5a2bd0445b9564d84a155b1196cb73d93d7d974
R0N <ron.muehlhaus@gmail.com>
2023-03-23 17:36:41 +0100

    load savedata

--- starthack/lib/main.dart
+++ starthack/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:starthack/Stock/Chart.dart';
 import 'package:starthack/routes.dart';
 import 'package:starthack/shared/theme.dart';
 import 'package:starthack/Stock/Stockpage.dart';
+import 'package:starthack/shared/Data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: appRoutes,
-      theme: appTheme,
-    );
+    return FutureBuilder(
+        future: loadWatchList(),
+        builder: (context, snapshot) {
+          return MaterialApp(
+            routes: appRoutes,
+            theme: appTheme,
+          );
+        });
   }
 }
