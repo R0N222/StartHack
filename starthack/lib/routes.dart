89a990ed6b3ddc1567f0fb028384793df9d2a630
R0N <ron.muehlhaus@gmail.com>
2023-03-23 06:08:36 +0100

    Homepage rework

--- starthack/lib/routes.dart
+++ starthack/lib/routes.dart
@@ -1,7 +1,8 @@
 import 'package:starthack/Homescreen/Homescreen.dart';
 import 'package:starthack/Stock/Stockpage.dart';
+import 'package:starthack/Homescreen/Homepage/UI/home_page.dart';
 
 var appRoutes = {
-  '/': (context) => const HomeScreenWidget(),
+  '/': (context) => const HomePage(title: "Home"),
   '/stock': (context) => const StockScreenWidget(),
 };
