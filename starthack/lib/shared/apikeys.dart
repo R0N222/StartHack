d9c8f1d01b9af2b502eeab85ce92023517c0a6db
R0N <ron.muehlhaus@gmail.com>
2023-03-24 10:11:51 +0100

    openai keeps banning my keys

--- starthack/lib/shared/apikeys.dart
+++ starthack/lib/shared/apikeys.dart
@@ -1,1 +1,5 @@
-const openAIKey = "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";
+const key1 = "sk-y6XPimfQIJ";
+const key2 = "z8xENH3o5cT3BlbkFJ";
+const key3 = "ozqsA5W1XtdZlEu1gMU5";
+
+const openAIKey = key1 + key2 + key3;
